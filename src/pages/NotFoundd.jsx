1448ef10494e6f67a8e8bfb6aad718d7a3e4a1bc
Pedro Wilson <pdro.wilson@gmail.com>
2023-01-22 21:01:35 -0300

    feat: update page 404

--- src/pages/NotFoundd.jsx
+++ src/pages/NotFoundd.jsx
@@ -4,8 +4,11 @@ import { Link } from 'react-router-dom'
 const NotFoundd = () => {
   return (
     <div className='flex justify-center flex-wrap mt-7'>
-      <p className='mr-1'>Oops... There is no card here!</p>
-      <Link className="text-blue-400 hover:underline" to={"/"}>See All Cards</Link>
+      <div>
+        <h1 className='font-medium text-5xl'>404</h1>
+        <p className='mr-1'>Oops... There is no card here!</p>
+        <Link className="text-blue-400 hover:underline" to={"/"}>See All Cards</Link>
+      </div>
     </div>
   )
 }
